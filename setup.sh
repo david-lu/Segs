80b84e92197e1c3eb984f1985715fa49700f0dec
David Lu <david-lu@hotmail.com>
2025-05-24 09:23:26 -0700

    a

--- setup.sh
+++ setup.sh
@@ -22,3 +22,8 @@ mkdir -p preproc/tapnet/checkpoints
   cd preproc/tapnet/checkpoints || exit 1
   wget https://storage.googleapis.com/dm-tapnet/bootstap/bootstapir_checkpoint_v2.pt
 )
+
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+pip install -r requirements.txt
+pip install -U xformers --index-url https://download.pytorch.org/whl/cu121
+wget -O models/moseg.pth https://huggingface.co/Changearthmore/moseg/resolve/main/moseg.pth

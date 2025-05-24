18f4ca02d0509d2f75b84819d63cc506c766491a
David Lu <david-lu@hotmail.com>
2025-05-24 09:10:43 -0700

    add

--- setup.sh
+++ setup.sh
@@ -1,42 +1,24 @@
-#!/bin/bash
-
-# Deinitialize all submodules
-git submodule deinit -f --all
-
-# Remove all submodule metadata
-rm -rf .git/modules/preproc/dinov2
-rm -rf .git/modules/preproc/tapnet
-rm -rf .git/modules/core/eval/davis2017-evaluation
-
-# Optionally remove their working directories (if broken)
-rm -rf preproc/dinov2
-rm -rf preproc/tapnet
-rm -rf core/eval/davis2017-evaluation
-
-git submodule deinit -f --all
-git submodule update --init --recursive --force
-
 #!/bin/sh
 set -e  # Exit on error
 
 # Install sam2
 (
-  cd sam2 || exit
+  cd sam2 || exit 1
   pip install -e .
 
-  cd checkpoints || exit
+  cd checkpoints || exit 1
   ./download_ckpts.sh
 )
 
 # Install tapnet
 (
-  cd preproc/tapnet || exit
+  cd preproc/tapnet || exit 1
   pip install .
 )
 
 # Download TapNet checkpoint
 mkdir -p preproc/tapnet/checkpoints
 (
-  cd preproc/tapnet/checkpoints || exit
+  cd preproc/tapnet/checkpoints || exit 1
   wget https://storage.googleapis.com/dm-tapnet/bootstap/bootstapir_checkpoint_v2.pt
 )
